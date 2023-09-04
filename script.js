75c17ef56943b61734b8e81b745d985c2550b44c
alejandralobaton <alejandralobaton@gmail.com>
2023-09-04 20:31:04 -0300

    subindo arquivos do portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+    }
+    });
+});
+
+
+$(document).ready(function() {
+    $(".menu-btn").click(function() {
+    $(this).toggleClass("active");
+    $(".menu").toggleClass("active");
+
+    });
+});
+
+
