ffcee10e56555136cf7cd5b50554ce78165b5676
alejandralobaton <alejandralobaton@gmail.com>
2023-09-11 19:36:37 -0300

    formulario funcionando

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// efeito da barra do menu
 
 $(document).ready(function(){
     $(window).scroll(function(){
@@ -10,6 +10,7 @@ $(document).ready(function(){
     });
 });
 
+//menu hamburguer
 
 $(document).ready(function() {
     $(".menu-btn").click(function() {
@@ -20,3 +21,82 @@ $(document).ready(function() {
 });
 
 
+//configuração do formulario
+
+class FormSubmit{
+  constructor(settings) {
+    this.settings = settings;
+    this.form= document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+    if (this.form){
+        this.url = this.form.getAttribute("action");
+
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  displaySuccess(){
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError(){
+    this.form.innerHTML = this.settings.error;
+
+  }
+
+  getFormObject(){
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]")
+    fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+
+    });
+    return formObject;
+  }
+
+  onSubmission(event){
+    event.preventDefault();
+    event.targer.disabled = true;
+    event.target.innerText = "Enviando...";
+
+  }
+
+
+async sendForm(event){
+   try {
+     this.onSubmission(event);
+     await fetch(this.url, {
+        method: "POST",
+        headers: {
+            "content-Type":"application/json",
+            Accept: "application/json",
+        },
+
+        body:  JSON.stringify(this.getFormObject()),
+
+    } );
+
+    this.displaySuccess();
+  } catch{
+     this.displayError();
+     throw new Error(error);
+
+  }
+ }
+
+
+  init(){
+    if (this.form) this.formButton.addEventListener("click", () => this.displaySuccess());
+    return this;
+  }
+
+}
+
+const formSubmit = new FormSubmit({
+        form: "[data-form]",
+        button: "[data-button]",
+        success: "<h1 class='success'>Mensagem enviada</h1>",
+        error: "<h1 class='error'> Não foi possivel enviar sua mensagem </h1>",
+}); 
+
+formSubmit.init();
